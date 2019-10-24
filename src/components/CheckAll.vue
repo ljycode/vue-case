8163983f6684395de2b2e9441907904fb89be229
ljycode <183158862@qq.com>
2019-10-24 21:54:19 +0800

    checkAll

---
+++ src/components/CheckAll.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+      <input type="checkbox" v-model="checkAll" /> 全选<br/>
+      <input type="checkbox" v-for="item in checks" :key="item.id" v-model="item.value" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckAll',
+  data() {
+    return {
+      checks: [{
+        id: 1,
+        value: true,
+        text: '选项一',
+      },
+      {
+        id: 2,
+        value: false,
+        text: '选项二',
+      },
+      {
+        id: 3,
+        value: true,
+        text: '选项三',
+      },
+      ],
+    };
+  },
+  computed: {
+    checkAll: {
+      get() {
+        return this.checks.every(item => item.value);
+      },
+      // 当全选的value值发生改变时，同步设置其他的值
+      set(value) {
+        this.checks.forEach((item) => {
+          item.value = value;
+        });
+      },
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <router-link :to="{name:'CheckAll'}">全选功能</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
