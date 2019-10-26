43fa0c0a4e335713cba93fa59468fb795261f0d5
ljycode <ljycode@163.com>
2019-10-27 01:10:44 +0800

    stopmove

--- src/components/BallBox.vue
+++ src/components/BallBox.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
-    <scroll-ball @input="input" :start="start1" :target="target1" color="blue"></scroll-ball>
-    <scroll-ball @input="input" :start="start2" :target="target2" color="red"></scroll-ball>
+    <!-- 1.通过props + $emit -->
+    <!-- <scroll-ball @input="input" :value="start1" :target="target1" color="blue">
+    </scroll-ball> -->
+    <!-- 2.将初始位置属性名又start改写为value后,可通过v-model实现双向数据绑定 -->
+    <!-- <scroll-ball v-model="start1" :target="target1" color="blue"></scroll-ball> -->
+    <!-- 3.通过.sync实现 -->
+    <scroll-ball :value.sync="start2" @input="input" :target="target2" color="red"></scroll-ball>
+    <!-- 相当于下边的，是下边的语法糖 -->
+    <scroll-ball ref="ball" :value="start2" @update:value="input"
+    :target="target2" color="red"></scroll-ball>
+    <button @click="stop">停止运动</button>
   </div>
 </template>
 
@@ -15,13 +24,18 @@ export default {
       start1: 50,
       target1: 200,
       start2: 10,
-      target2: 300,
+      target2: 500,
     };
   },
   methods: {
+    // 使用v-model实现双向数据绑定时，不需要这个方法
     input(value) {
       this.start1 = value;
       this.start2 = value;
+    },
+    stop() {
+      // 调用子组件实例的方法
+      this.$refs.ball.stop();
     },
   },
   components: {
--- src/components/ScrollBall.vue
+++ src/components/ScrollBall.vue
@@ -4,7 +4,12 @@
 </template>
 
 <script>
-// props + $emit 双向数据通信
+/*
+ 双向数据通信3种实现：
+ 1. props + $emit
+ 2. v-model = @input="input" + :value="" （此方法用的是最多的）
+ 3. .sync = :xxx="xxx" @update:xxx="" + 子组件$emit("update:value")
+*/
 export default {
   name: 'SrollBall',
   mounted() {
@@ -13,25 +18,33 @@ export default {
     let ball = document.getElementById(this.ballId);
     // eslint-disable-next-line consistent-return
     const fn = () => {
-      const left = this.start;
+      const left = this.value;
       if (left >= this.target) {
         // 取消定时器
         return cancelAnimationFrame(this.timer);
       }
-      // 将子组件的值传递给父组件，修改start的值
-      this.$emit('input', left + 5);
+      // 1.将子组件的值传递给父组件，修改父组件的start的值；
+      // 2.使用v-model实现双向数据绑定是也需要用$emit()
+      // this.$emit('input', left + 5);
+      // 3.通过.sync实现不使用上行的写法，而使用下行的
+      this.$emit('update:value', left + 5);
       ball.style.transform = `translate(${left}px)`;
       this.timer = requestAnimationFrame(fn);
     };
     // 类似setInterval，但比setInterval好用
     this.timer = requestAnimationFrame(fn);
   },
+  methods: {
+    stop() {
+      cancelAnimationFrame(this.timer);
+    },
+  },
   props: {
     color: {
       type: String,
       default: 'white',
     },
-    start: {
+    value: {
       type: Number,
       default: 0,
     },
